807082870ea745c3cf5971ce559f6e7efff15825
Rustem Arslanov <sobakasobaka6@gmail.com>
2021-09-11 17:28:59 +0500

    asd

--- App.js
+++ App.js
@@ -2,12 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen(){
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>My Home Screen</Text>
+  </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    /*
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working</Text>
       <StatusBar style="auto" />
     </View>
+    */
   );
 }
 
